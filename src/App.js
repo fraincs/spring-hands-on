9f6088fae429ed9e31548f251495ca329af2eb44
Francis Thibault <francis.thibault@gsoft.com>
2019-09-27 09:14:50 -0400

    class component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Spring from "./components/useSpring";
 import Transition from "./components/useTransition";
 import Inner from "./components/inner";
 import UseChain from "./components/useChain";
+import MyComponent from "./components/classComponent";
 
 import "./App.css";
 
@@ -11,18 +12,18 @@ function App() {
     <div className="App">
       <h1>useSpring</h1>
       <div className="area gradient-2 tc">
-        <Spring></Spring>
+        <Spring />
       </div>
       <h1>Inner Text</h1>
       <p className="tl mb4">
         Change this animation in order to make it animate from 0 to 100%.
       </p>
       <div className="area gradient-6 tc">
-        <Inner></Inner>
+        <Inner />
       </div>
       <h1>useTransition</h1>
       <div className="area gradient-3 tc">
-        <Transition></Transition>
+        <Transition />
       </div>
       <h1>Use Chain</h1>
       <p className="tl mb4">
@@ -30,7 +31,11 @@ function App() {
         then make the inner items appears (2 step animation)
       </p>
       <div className="area gradient-8 tc">
-        <UseChain></UseChain>
+        <UseChain />
+      </div>
+      <h1>Class components</h1>
+      <div className="area gradient-2 tc">
+        <MyComponent />
       </div>
     </div>
   );
